69d2e62284c81e51031b059de5d4f1482de784a5
GerardoNavaDionicio <gerardo.nava1829@uppuebla.edu.mx>
2022-08-18 13:43:19 -0500

    .
    .

---
+++ obtener_image/app/src/main/java/com/example/obtener_image/productos.java
@@ -0,0 +1,47 @@
+package com.example.obtener_image;
+
+import android.content.Context;
+
+import java.util.List;
+
+public class productos {
+
+
+    private int id;
+    private double precio;
+    private String nombre;
+    private String imagen;
+
+    public productos(int id,double precio, String nombre, String imagen)
+    {
+        this.id=id;
+        this.precio=precio;
+        this.nombre=nombre;
+        this.imagen=imagen;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getImagen() {
+        return imagen;
+    }
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+}
+
